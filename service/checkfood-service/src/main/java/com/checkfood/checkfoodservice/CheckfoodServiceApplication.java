98d08774ffee3369d973b80992eb0b79fe990a86
RJ-B <rosta1013@gmail.com>
2025-11-13 16:11:21 +0100

    Initial Spring Boot project setup

---
+++ service/checkfood-service/src/main/java/com/checkfood/checkfoodservice/CheckfoodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.checkfood.checkfoodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckfoodServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckfoodServiceApplication.class, args);
+    }
+
+}
---
+++ service/checkfood-service/src/test/java/com/checkfood/checkfoodservice/CheckfoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkfood.checkfoodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckfoodServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
